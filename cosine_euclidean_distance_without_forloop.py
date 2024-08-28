900d6f42a17755733d7a7e236d5f003488f23305
honlin96 <37786219+honlin96@users.noreply.github.com>
2024-08-28 16:34:49 +0800

    Add files via upload
    for stupid coding interviews

---
+++ cosine_euclidean_distance_without_forloop.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 14:24:09 2024
+
+@author: honlin
+"""
+
+import numpy as np
+import numpy.linalg as LA
+
+A = np.array([[1,2],[2,-2],[3,-1]])
+B = np.array([[2,3],[4,5],[3,-5],[4,6]])
+
+def cosine_similarity(A,B):
+    '''
+    A cosine similarity returns the pair-wise cosine 
+    angle between the 2 vectors
+    i.e. =  cos(theta) = v1.v2/|v1||v2|
+    A: n x d array
+    B: m x d array
+    
+    return: 
+        x = n x m array 
+        
+    '''
+
+    no = A @ B.T 
+    A_norm = np.array([LA.norm(A, axis = 1)])
+    B_norm = np.array([LA.norm(B, axis = 1)])
+    de = A_norm.T @ B_norm
+    cos = np.divide(no, de)
+    return cos
+
+cos = cosine_similarity(A,B)
+
+def euclidean_distance(A,B):
+    '''
+    euclidean_distance returns the pair-wise distance
+    between 2 vectors
+    i.e. C = A**2 - B**2
+
+    Parameters
+    ----------
+    A : n x d array
+    B : m x d array
+
+    Returns
+    -------
+    dist: n x m array
+
+    '''
+    pairwise_sub = A[:, np.newaxis, :]-B[np.newaxis, :, :]
+    dist = np.sqrt(np.sum(pairwise_sub**2, axis = 2))
+    return dist
+
+d = euclidean_distance(A,B)
